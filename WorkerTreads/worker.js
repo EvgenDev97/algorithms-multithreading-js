050666fe9f6c6fed6fe249f1adb2e60d0d9c5b1b
aenkas <mi69popov@gmail.com>
2024-01-07 19:11:56 +0300

    fix woerk

--- WorkerTreads/worker.js
+++ WorkerTreads/worker.js
@@ -1,13 +1,14 @@
-const {parentPort, workerData} = require("worker_threads")
+const { parentPort, workerData, isMainThread } = require("worker_threads");
 
 const factorial = require("./factorial");
-const compute = ({array})=>{
-    const arr = []
-    for( let i = 0; i < 10000000; i++){
-        arr.push(i*i)
+const compute = ({ array }) => {
+    const arr = [];
+    for (let i = 0; i < 10000000; i++) {
+        arr.push(i * i);
     }
-    console.log(arr)
-    return array.map(item => factorial(item))
-}
+    return array.map((item) => factorial(item));
+};
 
-parentPort.postMessage(compute(workerData))
+if (!isMainThread) {
+    parentPort.postMessage(compute(workerData));
+}
