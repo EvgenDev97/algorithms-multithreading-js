35623765f455ceedc99940e86b941bd4bd17ca1f
evgen <evgendev2022@gmail.com>
2024-01-08 19:52:47 +0300

    fork async

---
+++ WorkerTreads/Fork/index.js
@@ -0,0 +1,17 @@
+ const {fork} = require("child_process")
+ const forkProcess = fork('fork.js')
+
+ forkProcess.on('message', (msg)=>{
+     console.log(msg)
+ })
+
+forkProcess.on('close', (code)=>{
+    console.log(code)
+})
+
+ forkProcess.on("error", (err)=> {
+     console.log(err)
+ })
+
+ forkProcess.send("Ping")
+ forkProcess.send("disconnect")
